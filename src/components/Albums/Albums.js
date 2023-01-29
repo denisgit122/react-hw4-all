00136e2fe5e4c1a932fc764b08603aaca44d9d6b
denisgit122 <yaholnykden@gmail.com>
2023-01-29 22:32:39 +0200

    hw4

--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -1,17 +1,28 @@
 import {useEffect, useState} from "react";
 
-import {baseURL, urls} from "../../configs/urls";
+import {album, baseURL, urls} from "../../configs/urls";
 import {Album} from "./Album";
 import './albumsSt.css'
 
 const Albums = () => {
  const [albums,setAlbums]=  useState([])
 
+    const [totalPage,setTotalPage]=useState(0)
+    const [limit,setLimit]=useState(10)
+    const [page,stPage]=useState(1)
+
     useEffect(() => {
-urls.albums(10,1).then(({data})=>setAlbums(data))
+// urls.albums(10,1).then(({data})=>setAlbums(data))
+     fechAmbum()
+    },[page])
 
-    },[])
+async function fechAmbum(){
+   await album.albums(limit,page).then(({data})=>setAlbums(data))
 
+     // const re=totakCaunt.headers['x-total-count']
+    }
+
+    console.log();
     // console.log(albums);
 return (
   <div className={'albums'}>
--- src/configs/urls.js
+++ src/configs/urls.js
@@ -3,13 +3,18 @@ import axios from "axios";
 const  baseURL = axios.create({baseURL:'https://jsonplaceholder.typicode.com'});
 const urls ={
     todos:'/todos',
-    albums:(limit=10,page=1)=>baseURL.get('/albums',{
-        _limit:limit,
-        _page:page
-    }),
+
     comment:'/comments',
 }
 const postss={
     getById:(id)=>baseURL.get(`/posts/${id}`)
 }
-export {urls, baseURL, postss};
+const album={
+    albums:(limit=10,
+            page=1
+    )=>baseURL.get('/albums', {
+        _limit: limit,
+        _page: page
+    })
+}
+export {urls, baseURL, postss,album};
